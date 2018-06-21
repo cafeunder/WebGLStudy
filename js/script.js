673d2242e05bdfcd0959b93e38a7e452a8110340
cafeunder <cafe3641@gmail.com>
2018-06-21 12:49:47 +0900

    頂点に色を追加

--- js/script.js
+++ js/script.js
@@ -1,55 +1,63 @@
 
 onload = function(){
-    // canvasエレメントを取得
+    // === initialize === //
+    // initialize canvas
     var c = document.getElementById('mycanvas');
     c.width = 300;
     c.height = 300;
 
-    // webglコンテキストを取得
+    // initialize WebGL
     var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
-    
-    // canvasを初期化する色を設定する
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    
-    // canvasを初期化する際の深度を設定する
-    gl.clearDepth(1.0);
-    
-    // canvasを初期化
+    gl.clearDepth(1.0); // clearColorでクリアされる深度を設定
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
-    // 頂点シェーダとフラグメントシェーダの生成
+
+    // create vertex/fragment shader
     var v_shader = create_shader('vshader');
     var f_shader = create_shader('fshader');
-    
-    // プログラムオブジェクトの生成とリンク
     var prg = create_program(v_shader, f_shader);
-    
-    // attributeLocationの取得
-    var attLocation = gl.getAttribLocation(prg, 'position');
-    
-    // attributeの要素数(この場合は xyz の3要素)
-    var attStride = 3;
-    
-    // モデル(頂点)データ
+
+
+    // === model definition === //
+    var attLocation = new Array(2);
+    attLocation[0] = gl.getAttribLocation(prg, 'position');
+    attLocation[1] = gl.getAttribLocation(prg, 'color');
+
+    var attStride = new Array(2);
+    attStride[0] = 3;
+    attStride[1] = 4;
+
+    // 頂点データ
     var vertex_position = [
-         0.0, 1.0, 0.0,
-         1.0, 0.0, 0.0,
-        -1.0, 0.0, 0.0
+        // x y z
+        0.0, 1.0, 0.0, // v1
+        1.0, 0.0, 0.0, // v2
+        -1.0, 0.0, 0.0  // v3
     ];
     
-    // VBOの生成
-    var vbo = create_vbo(vertex_position);
-    
-    // VBOをバインド
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-    
+    var vertex_color = [
+        // r g b a
+        1.0, 0.0, 0.0, 1.0, // v1
+        0.0, 1.0, 0.0, 1.0, // v2
+        0.0, 0.0, 1.0, 1.0, // v3
+    ];
+
+    // position VBOの生成
+    var position_vbo = create_vbo(vertex_position);
+    // 操作を行うためにVBOを再バインド
+    gl.bindBuffer(gl.ARRAY_BUFFER, position_vbo);
     // attribute属性を有効にする
-    gl.enableVertexAttribArray(attLocation);
-    
+    gl.enableVertexAttribArray(attLocation[0]);
     // attribute属性を登録
-    gl.vertexAttribPointer(attLocation, attStride, gl.FLOAT, false, 0, 0);
-    
-    // minMatrix.js を用いた行列関連処理
+    gl.vertexAttribPointer(attLocation[0], attStride[0], gl.FLOAT, false, 0, 0);
+
+    // color VBOの生成
+    var color_vbo = create_vbo(vertex_color);
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_vbo);
+    gl.enableVertexAttribArray(attLocation[1]);
+    gl.vertexAttribPointer(attLocation[1], attStride[1], gl.FLOAT, false, 0, 0);
+
+    // === transform === //
     // matIVオブジェクトを生成
     var m = new matIV();
     
@@ -59,25 +67,24 @@ onload = function(){
     var pMatrix = m.identity(m.create());
     var mvpMatrix = m.identity(m.create());
     
-    // ビュー座標変換行列
+    // view transform
     m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
-    
-    // プロジェクション座標変換行列
+    // projection transform
     m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
-    
-    // 各行列を掛け合わせ座標変換行列を完成させる
+
+    // create transform matrix
     m.multiply(pMatrix, vMatrix, mvpMatrix);
     m.multiply(mvpMatrix, mMatrix, mvpMatrix);
-    
+
     // uniformLocationの取得
     var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
-    
     // uniformLocationへ座標変換行列を登録
     gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-    
+
+
+    // === drawing === //
     // モデルの描画
     gl.drawArrays(gl.TRIANGLES, 0, 3);
-    
     // コンテキストの再描画
     gl.flush();
 
@@ -97,17 +104,17 @@ onload = function(){
             default :
                 return;
         }
-    
+
         gl.shaderSource(shader, scriptElement.text);
         gl.compileShader(shader);
-    
+
         if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
             return shader;
         } else {
             alert(gl.getShaderInfoLog(shader));
         }
     }
-    
+
     function create_program(vs, fs){
         var program = gl.createProgram();
     
